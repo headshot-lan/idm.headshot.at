792580d8f4ba72d1c4dc50ae5e4e258b24facebf
Clemens D <firegore@users.noreply.github.com>
2020-06-21 21:45:00 +0200

    fix Birthdate

--- src/Form/UserEditType.php
+++ src/Form/UserEditType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -30,7 +31,7 @@ class UserEditType extends AbstractType
             ->add('hardware')
             ->add('infoMails')
             ->add('statements')
-            ->add('birthdate')
+            ->add('birthdate', BirthdayType::class)
         ;
     }
 
