677dab38cc8d2b18a60586bfc81cb73257a7191e
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2023-12-20 22:41:10 +0100

    chore: freedns update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,11 @@ set +e
 
 BASE_DIR=`dirname $0`
 
-
-rsync -avzh --exclude='/.env' --exclude='/.git' --exclude='/node_modules' * -e "ssh -p 822" headshot_ftp@idm.headshot.at:/idm.headshot.at/
+echo "Updating dyndns and sleep for 5 seconds"
+# curl --insecure https://freedns.afraid.org/dynamic/update.php?N1ROamozNmlTaEgydkM3M3lYNjM6MTU2NTc4OTY=
+curl --insecure https://carol.selfhost.de/update?username=303092&password=GegJabCij2&textmodi=1
+sleep 10
+echo "Transfering data"
+rsync -avzh --exclude='/.env' --exclude='/.git' --exclude='/node_modules' * -e "ssh -p 822 -o ConnectTimeout=5" headshot_ftp@kdn10.futureweb.at:/idm.headshot.at/
 echo "Rsynced data"
 
